6b9b7073142afa75208ef8b744beed08e68e48cd
sabrina2712 <mailroshni@gmail.com>
2020-07-17 11:02:57 +0200

    changed onclick handler

--- src/components/content.js
+++ src/components/content.js
@@ -5,51 +5,57 @@ import "./style.css"
 
 
 
+
 class CollapsibleContent extends React.Component{
-        constructor(props){
-            super(props);
-            this.state = {data : data, isOpen: false, showMenu: null}
+    constructor(props){
+        super(props);
+        this.state = {data : data, isOpen: false, showMenu: null}
 
-        }
-
-        onClickHandler=(key)=>{
-            let {isOpen,showMenu} = this.state
-            this.setState({
-                isOpen: !this.state.isOpen,
-                 showMenu: key 
-            })
-        }
-
-      render(){
-      
-        return( 
-            <>
-               
-                <h1>Collapsible List</h1>
-                <ul>
-                {data.map((item, index) => (
-                <div key={index}>
-                   <div key={item.id}>
-                        <div className="list-items"  onClick ={this.onClickHandler}>
-  
-                        {item.question} <span span-item>+</span></div>
-                        <div className="answer-item"  style={this.state.isOpen ? {} :{ display: 'none' }}
-                        
-                        >{item.answer}</div>
-                        
-                    </div>
-                   
-                </div>
-                ))}
-            </ul>
-
-                    
-            </>
-        )
     }
+
+    onClickHandler=(key)=>{
+        let {showMenu} = this.state
+        this.setState({
+          
+             showMenu: key
+        })
+    }
+
+
+  render(){
+  
+    return( 
+        <>
+           
+            <h1>Collapsible List</h1>
+            <ul>
+            {data.map((item, index) => (
+            <div key={item.id}>
+               <div key={item.id}>
+                    <div className="list-items"  onClick ={()=>{
+                        this.onClickHandler(item.id)
+                    }}>
+
+                    {item.question} <span span-item>+</span></div>
+                    <div className="answer-item"  style={this.state.showMenu === item.id? {} :{ display: 'none' }}
+                    
+                    >{item.answer}</div>
+                    
+                </div>
+               
+            </div>
+            ))}
+        </ul>
+
+                
+        </>
+    )
+}
 }
 
 
 
 
 export default CollapsibleContent;
+
+
